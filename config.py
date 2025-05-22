af245a2d9aa0b809cac312f278efb33cd3cde955
peuBouzon <peubouzon@gmail.com>
2025-05-22 18:23:07 -0300

    add config file

---
+++ config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PAD_20_PATH = Path("/home/pbouzon/datasets/pad-ufes-20")
+PAD_20_IMAGES_FOLDER = PAD_20_PATH / "images"
+PAD_20_ONE_HOT_ENCODED = PAD_20_PATH / "pad-ufes-20-one-hot-demo.csv"
+PAD_20_RAW_METADATA = PAD_20_PATH / "metadata.csv"
+PAD_20_SENTENCE_ENCODED = PAD_20_PATH / "pad-ufes-20-sentence-demo.csv"
+PAD_20_BAYESIAN_DATA = PAD_20_PATH / 'bayesian'
